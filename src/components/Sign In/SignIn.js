482f70ab3b1e7ecfbb45b6fcf63aa3cf6ce648cd
Shahadat91 <shahadat20000@gmail.com>
2022-04-18 11:45:16 +0600

    sign up form created and style it

--- src/components/Sign In/SignIn.js
+++ src/components/Sign In/SignIn.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useRef } from "react";
+import {useNavigate } from "react-router-dom";
+import "./SignIn.css";
 
 const SignIn = () => {
-    return (
-        <div style={{marginTop: '100px'}}>
-            <h2>This is Sign In</h2>
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+  };
+
+  const navigateSignUp = (event) => {
+    navigate("/signup");
+    console.log("signup");
+  };
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form onSubmit={handleSignIn}>
+        <div className="login-form">
+          <div className="title">Please Sign In</div>
+
+          <div className="input-fields">
+            <input
+              className="username"
+              ref={emailRef}
+              type="email"
+              name="email"
+              id=""
+              placeholder="your email"
+              required
+            />
+
+            <input
+              className="password"
+              ref={passwordRef}
+              type="password"
+              name="password"
+              id=""
+              placeholder="password"
+              required
+            />
+          </div>
+          <button className="button">Sign In</button>
+      <div className="link">
+        <p>
+          New at this site? Please{" "}
+          <span className="link-a" onClick={navigateSignUp}>
+            SignUp
+          </span>
+        </p>
+        <p>
+          Forgot Password? <span className="link-a" >Reset Password</span>
+        </p>
+      </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
--- src/components/Sign Up/SignUp.js
+++ src/components/Sign Up/SignUp.js
@@ -1,11 +1,73 @@
-import React from 'react';
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>This is Sign Up</h2>
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+    const name = nameRef.current.value
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(name,email, password);
+  };
+
+  const navigateSignIn = (event) => {
+    navigate("/signin");
+    console.log("signin");
+  };
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form onSubmit={handleSignUp}>
+        <div className="login-form">
+          <div className="title">Please Sign Up</div>
+
+          <div className="input-fields">
+            <input
+              className="username"
+              ref={nameRef}
+              type="text"
+              name="name"
+              id=""
+              placeholder="your name"
+              required
+            />
+            <input
+              className="username"
+              ref={emailRef}
+              type="email"
+              name="email"
+              id=""
+              placeholder="your email"
+              required
+            />
+
+            <input
+              className="password"
+              ref={passwordRef}
+              type="password"
+              name="password"
+              id=""
+              placeholder="password"
+              required
+            />
+          </div>
+          <button className="button">Sign In</button>
+          <div className="link">
+            <p>
+              Already have an account? Please{" "}
+              <span className="link-a" onClick={navigateSignIn}>
+                Sign In
+              </span>
+            </p>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
